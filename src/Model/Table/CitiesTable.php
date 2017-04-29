302615226f35d50fcc9c4c7eab243c655e1f86eb
erick <erick@erick-HP>
2017-04-29 19:44:31 -0400

    corregido el use_created y el use_modifilied

--- src/Model/Table/CitiesTable.php
+++ src/Model/Table/CitiesTable.php
@@ -74,16 +74,6 @@ class CitiesTable extends Table
             ->boolean('published')
             ->allowEmpty('published');
 
-        $validator
-            ->integer('user_created')
-            ->requirePresence('user_created', 'create')
-            ->notEmpty('user_created');
-
-        $validator
-            ->integer('user_modified')
-            ->requirePresence('user_modified', 'create')
-            ->notEmpty('user_modified');
-
         return $validator;
     }
 
