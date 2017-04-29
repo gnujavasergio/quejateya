20e2d33e6327ad7c0edc30fe321e4399a109fc68
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2017-04-29 16:40:25 -0400

    AdminLTE

--- config/app.php
+++ config/app.php
@@ -229,9 +229,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'root',
+            'password' => '5653651',
+            'database' => 'upds_bdquejateya',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
